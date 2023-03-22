f9ea87dee6a57de093f36fd5316ae13ce391f0f5
Leanne Vu <leannevu1@yahoo.com>
2023-03-22 06:23:32 +0000

    All longest strings algorithm.

---
+++ algorithms/all-longest-strings.js
@@ -0,0 +1,16 @@
+function solution(inputArray) {
+  let longboys = [];
+  let max = 0;
+  for (const str of inputArray) {
+    if (str.length > max) {
+      longboys = [];
+      max = str.length;
+      longboys.push(str);
+    } else if (str.length === max) {
+      longboys.push(str);
+    }
+  }
+  return longboys;
+}
+
+solution();
