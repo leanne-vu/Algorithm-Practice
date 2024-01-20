70626b4e5df01bb3748ffc799d7a3426f303af06
Leanne Vu <leannevu1@yahoo.com>
2024-01-19 17:20:06 -0800

    Mod.

--- algorithms/is-beautiful-string.js
+++ algorithms/is-beautiful-string.js
@@ -15,7 +15,7 @@
 // For inputString = "bbc", the output should be solution(inputString) = false.
 
 // Although there are more bs than cs, this string is not beautiful because there are no as, so therefore there are more bs than as.
-
+//
 function solution(inputString) {
   return inputString.split('').sort().join('').replace(/([a-z])\1*/g, (it) => it.length + ',').split(',').slice(0, -1).map(Number).every((el, i, ar) => (inputString.indexOf(String.fromCharCode(i + 'a'.charCodeAt(0))) >= 0 && (i == 0 || ar[i] <= ar[i - 1])))
 
