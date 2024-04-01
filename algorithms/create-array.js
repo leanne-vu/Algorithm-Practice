01ca88693e6206f80124bef1abdfc58193ed81ef
Leanne Vu <leannevu1@yahoo.com>
2024-03-31 22:48:04 -0700

    create array algorithm.

---
+++ algorithms/create-array.js
@@ -0,0 +1,27 @@
+// Given an integer size, return array of length size filled with 1s.
+
+//   Example
+
+// For size = 4, the output should be
+// solution(size) = [1, 1, 1, 1].
+
+//   Input / Output
+
+//   [execution time limit]4 seconds(js)
+
+//   [memory limit]1 GB
+
+//   [input] integer size
+
+// A positive integer.
+
+// Guaranteed constraints:
+// 1 ≤ size ≤ 1000.
+
+// [output] array.integer
+
+function solution(size) {
+  return Array(size).fill(1);
+}
+
+solution()
