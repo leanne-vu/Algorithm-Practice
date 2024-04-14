16e267b4551afc62c11fc78c6ed243b2f482d22d
Leanne Vu <leannevu1@yahoo.com>
2024-04-13 23:35:48 -0700

    Update different-squares.js

--- algorithms/different-squares.js
+++ algorithms/different-squares.js
@@ -1,9 +1,6 @@
 // Given a rectangular matrix containing only digits, calculate the number of different 2 × 2 squares in it.
-
 //   Example
-
 // For
-
 // matrix = [[1, 2, 1],
 // [2, 2, 2],
 // [2, 2, 2],
