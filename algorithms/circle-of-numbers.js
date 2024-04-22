de0a0bed58a6d9ad4bbd527c03c249a93ed2f8cb
Leanne Vu <leannevu1@yahoo.com>
2024-04-21 23:57:47 -0700

    Update circle-of-numbers.js

--- algorithms/circle-of-numbers.js
+++ algorithms/circle-of-numbers.js
@@ -3,7 +3,6 @@
 // Given n and firstNumber, find the number which is written in the radially opposite position to firstNumber.
 
 //   Example
-
 // For n = 10 and firstNumber = 2, the output should be
 // solution(n, firstNumber) = 7.
 function solution(n, firstNumber) {
