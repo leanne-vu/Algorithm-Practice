b707b49e2f6714588b32b27f3b9516b5c33f4690
Leanne Vu <leannevu1@yahoo.com>
2024-04-25 23:26:33 -0700

    Update second-rightmost-zero-bit.js

--- algorithms/second-rightmost-zero-bit.js
+++ algorithms/second-rightmost-zero-bit.js
@@ -2,7 +2,6 @@
 // Presented with the integer n, find the 0 - based position of the second rightmost zero bit in its binary representation(it is guaranteed that such a bit exists), counting from right to left.
 
 // Return the value of 2position_of_the_found_bit.
-
 //   Example
 
 // For n = 37, the output should be
