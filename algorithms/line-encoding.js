572cadf26a41f3c2d9e302ed536c2857426db243
Leanne Vu <leannevu1@yahoo.com>
2024-04-26 23:55:53 -0700

    Update line-encoding.js

--- algorithms/line-encoding.js
+++ algorithms/line-encoding.js
@@ -1,5 +1,4 @@
 // Given a string, return its encoding defined as follows:
-
 // First, the string is divided into the least possible number of disjoint substrings consisting of identical characters
 // for example, "aabbbc" is divided into["aa", "bbb", "c"]
 // Next, each substring with length greater than one is replaced with a concatenation of its length and the repeating character
