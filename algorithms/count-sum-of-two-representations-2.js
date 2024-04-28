5926914a1a7bd6357490985edcd44d6c8ff77041
Leanne Vu <leannevu1@yahoo.com>
2024-04-27 23:58:30 -0700

    Update count-sum-of-two-representations-2.js

--- algorithms/count-sum-of-two-representations-2.js
+++ algorithms/count-sum-of-two-representations-2.js
@@ -1,7 +1,6 @@
 // Given integers n, l and r, find the number of ways to represent n as a sum of two integers A and B such that l ≤ A ≤ B ≤ r.
 
 //   Example
-
 // For n = 6, l = 2, and r = 4, the output should be
 // solution(n, l, r) = 2.
 
