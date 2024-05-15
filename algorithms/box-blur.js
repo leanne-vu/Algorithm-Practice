fd7ca6aa6d31d0f3112f5bfc04c7e4d1e9d62b3d
Leanne Vu <leannevu1@yahoo.com>
2024-05-14 23:52:20 -0700

    Update box-blur.js

--- algorithms/box-blur.js
+++ algorithms/box-blur.js
@@ -4,7 +4,6 @@
 // The pixels in the input image are represented as integers.The algorithm distorts the input image in the following way: Every pixel x in the output image has a value equal to the average value of the pixel values from the 3 × 3 square that has its center at x, including x itself.All the pixels on the border of x are then removed.
 
 // Return the blurred image as an integer, with the fractions rounded down.
-
 //   Example
 
 // For
