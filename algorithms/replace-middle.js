c81284d439eaac570b11182f5103db69039db3e9
Leanne Vu <leannevu1@yahoo.com>
2024-05-15 22:59:28 -0700

    Update replace-middle.js

--- algorithms/replace-middle.js
+++ algorithms/replace-middle.js
@@ -3,7 +3,6 @@
 // if arr contains an odd number of elements, its middle is the element whose index number is the same when counting from the beginning of the array and from its end;
 // if arr contains an even number of elements, its middle is the sum of the two elements whose index numbers when counting from the beginning and from the end of the array differ by one.
 // Given array arr, your task is to find its middle, and, if it consists of two elements, replace those elements with the value of middle.Return the resulting array as the answer.
-
 //   Example
 
 // For arr = [7, 2, 2, 5, 10, 7], the output should be
