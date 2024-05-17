e40ef436c47f37b8176f632a35fcdf5163ebb08a
Leanne Vu <leannevu1@yahoo.com>
2024-05-16 23:54:59 -0700

    Update least-factorial.js

--- algorithms/least-factorial.js
+++ algorithms/least-factorial.js
@@ -3,7 +3,6 @@
 // k = m!(where m! = 1 * 2 * ... * m) for some integer m;
 // k >= n.
 // In other words, find the smallest factorial which is not less than n.
-
 //   Example
 
 // For n = 17, the output should be
