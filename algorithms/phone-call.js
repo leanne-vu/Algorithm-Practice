635f9701fcdb393e46c7a6f6df4331c4d8f192ed
Leanne Vu <leannevu1@yahoo.com>
2024-05-20 23:39:53 -0700

    Update phone-call.js

--- algorithms/phone-call.js
+++ algorithms/phone-call.js
@@ -4,7 +4,6 @@
 //   each minute from the 2nd up to 10th(inclusive) costs min2_10 cents
 // each minute after 10th costs min11 cents.
 // You have s cents on your account before the call.What is the duration of the longest call(in minutes rounded down to the nearest integer) you can have ?
-
 //   Example
 
 // For min1 = 3, min2_10 = 1, min11 = 2, and s = 20, the output should be
