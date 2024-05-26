7922871e82944495dd4cff48217005d1bce8ad0b
Leanne Vu <leannevu1@yahoo.com>
2024-05-25 23:57:46 -0700

    Update deposit-profit.js

--- algorithms/deposit-profit.js
+++ algorithms/deposit-profit.js
@@ -1,5 +1,4 @@
 // You have deposited a specific amount of money into your bank account.Each year your balance increases at the same growth rate.With the assumption that you don't make any additional deposits, find out how long it would take for your balance to pass a specific threshold.
-
 // Example
 
 // For deposit = 100, rate = 20, and threshold = 170, the output should be
