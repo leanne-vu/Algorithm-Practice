1dac0ccb6fe469b10c0c4463215e7b585341a14e
Leanne Vu <leannevu1@yahoo.com>
2024-05-28 23:52:32 -0700

    Update extra-number.js

--- algorithms/extra-number.js
+++ algorithms/extra-number.js
@@ -1,5 +1,4 @@
 // You're given three integers, a, b and c. It is guaranteed that two of these integers are equal to each other. What is the value of the third integer?
-
 // Example
 
 // For a = 2, b = 7, and c = 2, the output should be
