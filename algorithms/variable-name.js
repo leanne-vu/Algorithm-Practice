7a1afa7f4f3330cf78abdf0c03d13356c3d1f28d
Leanne Vu <leannevu1@yahoo.com>
2024-06-03 23:47:46 -0700

    Update variable-name.js

--- algorithms/variable-name.js
+++ algorithms/variable-name.js
@@ -1,7 +1,6 @@
 // Correct variable names consist only of English letters, digits and underscores and they can't start with a digit.
 
 // Check if the given string is a correct variable name.
-
 //   Example
 
 // For name = "var_1__Int", the output should be
