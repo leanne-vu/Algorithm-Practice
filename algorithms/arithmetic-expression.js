509f6f52ce8f8952f5000f3dc9a2cbb08e57071c
Leanne Vu <leannevu1@yahoo.com>
2024-06-19 23:47:52 -0700

    Update arithmetic-expression.js

--- algorithms/arithmetic-expression.js
+++ algorithms/arithmetic-expression.js
@@ -1,5 +1,4 @@
 // Consider an arithmetic expression of the form a#b = c.Check whether it is possible to replace # with one of the four signs: +, -, * or / to obtain a correct expression.
-
 //   Example
 
 // For a = 2, b = 3, and c = 5, the output should be arithmeticExpression(a, b, c) = true.
