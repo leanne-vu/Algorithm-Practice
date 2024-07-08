2339f56d8d13962a4923bba43a8e279048462adf
Leanne Vu <leannevu1@yahoo.com>
2024-07-07 23:56:35 -0700

    Update chess-knight.js

--- algorithms/chess-knight.js
+++ algorithms/chess-knight.js
@@ -3,7 +3,6 @@
 // The knight can move to a square that is two squares horizontally and one square vertically, or two squares vertically and one square horizontally away from it.The complete move therefore looks like the letter L.Check out the image below to see all valid moves for a knight piece that is placed on one of the central squares.
 
 
-
 //   Example
 
 // For cell = "a1", the output should be
