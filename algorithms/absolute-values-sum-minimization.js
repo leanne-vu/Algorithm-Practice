a290ccfc7b01cdf7e8bcecd177563cd5de88e5da
Leanne Vu <leannevu1@yahoo.com>
2024-07-20 23:57:05 -0700

    Update absolute-values-sum-minimization.js

--- algorithms/absolute-values-sum-minimization.js
+++ algorithms/absolute-values-sum-minimization.js
@@ -2,7 +2,6 @@
 
 // abs(a[0] - x) + abs(a[1] - x) + ... + abs(a[a.length - 1] - x)
 //   (where abs denotes the absolute value)
-
 // If there are several possible answers, output the smallest one.
 //   Example
 
