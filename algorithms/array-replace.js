44e6af8e2ad1c0b65c65a13bcb30318b0833ec4f
Leanne Vu <leannevu1@yahoo.com>
2024-08-01 23:44:24 -0700

    Update array-replace.js

--- algorithms/array-replace.js
+++ algorithms/array-replace.js
@@ -1,5 +1,4 @@
 // Given an array of integers, replace all the occurrences of elemToReplace with substitutionElem.
-
 //   Example
 
 // For inputArray = [1, 2, 1], elemToReplace = 1, and substitutionElem = 3, the output should be
