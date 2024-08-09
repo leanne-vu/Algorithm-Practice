addd1b1863fb2d152251c46f5a0f82dc71105c15
Leanne Vu <leannevu1@yahoo.com>
2024-08-08 23:39:32 -0700

    Update adjacent-elements-product.js

--- algorithms/adjacent-elements-product.js
+++ algorithms/adjacent-elements-product.js
@@ -1,5 +1,4 @@
 // Given an array of integers, find the pair of adjacent elements that has the largest product and return that product.
-
 //   Example
 // For inputArray = [3, 6, -2, -5, 7, 3], the output should be
 // solution(inputArray) = 21.
