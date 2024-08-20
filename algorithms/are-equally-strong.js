eb7ffb604ed6bb70fc55d15deff5fa959748aa96
Leanne Vu <leannevu1@yahoo.com>
2024-08-19 23:59:17 -0700

    Update are-equally-strong.js

--- algorithms/are-equally-strong.js
+++ algorithms/are-equally-strong.js
@@ -1,7 +1,6 @@
 // Call two arms equally strong if the heaviest weights they each are able to lift are equal
 
 // Call two people equally strong if their strongest arms are equally strong(the strongest arm can be both the right and the left), and so are their weakest arms.
-
 // Given your and your friend's arms' lifting capabilities find out if you two are equally strong.
 //   Example
 // For yourLeft = 10, yourRight = 15, friendsLeft = 15, and friendsRight = 10, the output should be
