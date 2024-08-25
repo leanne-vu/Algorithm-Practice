0c3f2c76de5959d8a900b4036fdce4427fcb6461
Leanne Vu <leannevu1@yahoo.com>
2024-08-24 23:59:34 -0700

    Update array-max-consecutive-sum.js

--- algorithms/array-max-consecutive-sum.js
+++ algorithms/array-max-consecutive-sum.js
@@ -1,5 +1,4 @@
 // Given array of integers, find the maximal possible sum of some of its k consecutive elements.
-
 //   Example
 
 // For inputArray = [2, 3, 5, 1, 6] and k = 2, the output should be
