e29c92696e4a3790fea73473e375b346a08083dd
Leanne Vu <leannevu1@yahoo.com>
2024-09-15 23:52:22 -0700

    Update chess-board-cell-color.js

--- algorithms/chess-board-cell-color.js
+++ algorithms/chess-board-cell-color.js
@@ -1,5 +1,4 @@
 // Given two cells on the standard chess board, determine whether they have the same color or not.
-
 //   Example
 // For cell1 = "A1" and cell2 = "C3", the output should be
 // solution(cell1, cell2) = true.
